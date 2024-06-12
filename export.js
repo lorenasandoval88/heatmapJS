4b7d48d991406f85ae62dc1bebe4b3226160028d
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2024-06-11 22:30:57 -0400

    added  svg

--- export.js
+++ export.js
@@ -1,21 +1,38 @@
+
+import * as d3 from "https://cdn.skypack.dev/d3@7"
+
 function ui(targetDiv = document.body) {
     // target div for the user interface
-    //console.log(`prsCalc module imported at ${Date()}`)
+ 
     if (typeof (targetDiv) == 'string') {
         targetDiv = getElementById('targetDiv')
     }
-    //console.log(pgs)
     let div = document.createElement('div')
     targetDiv.appendChild(div)
     div.id = 'prsCalcUI'
-    div.innerHTML = `
-    <p>
-    HeatmapJS
-    </p>
-     <hr>
+    // div.innerHTML = `
+    // <p>
+    // HeatmapJS
+    // </p>
+    //  <hr>
     `
+
+    var theSVG = d3.select('prsCalcUI')
+    .append("svg")
+    .attr("width",200)
+    .attr("height",200);
+
+    theSVG.append("circle")
+    .style("stroke","green")
+    .style("fill","white")
+    .attr("r",40)
+    .attr("cx", 100)
+    .attr("cy", 100)
 }
 
+
+
 export {
+    d3,
     ui
 }
