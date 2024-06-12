5b931ec0a026d7c720c540e0f609067eaf44fde6
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2024-06-11 23:19:25 -0400

    Update export.js

--- export.js
+++ export.js
@@ -16,18 +16,21 @@ function ui(targetDiv = document.body) {
     // </p>
     //  <hr>
     //`
+    const svgCode = '<svg viewBox="0 0 100 100"><circle cx="50" cy="50" r="50"/></svg>';
+    const svgContainer = document.getElementById('svg-container');
 
-    var theSVG = d3.select('prsCalcUI')
-    .append("svg")
-    .attr("width",200)
-    .attr("height",200);
+    svgContainer.innerHTML = svgCode;
+    // var theSVG = d3.select('#prsCalcUI')
+    // .append("svg")
+    // .attr("width",200)
+    // .attr("height",200);
 
-    theSVG.append("circle")
-    .style("stroke","green")
-    .style("fill","white")
-    .attr("r",40)
-    .attr("cx", 100)
-    .attr("cy", 100)
+    // theSVG.append("circle")
+    // .style("stroke","green")
+    // .style("fill","white")
+    // .attr("r",40)
+    // .attr("cx", 100)
+    // .attr("cy", 100)
 }
 
 
